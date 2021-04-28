1c6e34d6fef2c2248ec221b95c762b4d44683725
Denisse <dm464@njit.edu>
2021-04-28 11:46:42 -0400

    Create file structure & router with navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Today, Week } from './pages';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Navbar } from './components';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" render={() => <Today />} exact />
+          <Route path="/week" render={() => <Week />} exact />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
